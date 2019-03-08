4501966c1eb642a0b12670168c685c17fdeb5b2e
Michael Bovee <michael.j.bovee@gmail.com>
2019-03-07 17:54:30 -0700

    added legend

--- main.js
+++ main.js
@@ -93,3 +93,20 @@ info.update = function(props) {
 }
 
 info.addTo(map)
+
+var legend = L.control({position: 'bottomright'})
+
+legend.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+    grades = [1, 3, 5, 7, 9],
+    labels = []
+    div.innerHTML = '<strong>Number of <br> Picnic Tables</strong><br><br>'
+
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML += '<i style="background:' + getColor(grades[i] + 1) + '"></i>' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+')
+    }
+
+    return div
+}
+
+legend.addTo(map)
